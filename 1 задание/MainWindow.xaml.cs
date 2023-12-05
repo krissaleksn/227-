e57ab0ba3ba57cdb8d3a79ed6dac12268da7d0b1
krissaleksn <151570370+krissaleksn@users.noreply.github.com>
2023-12-05 10:40:51 +0300

    1 задание

---
+++ 1 задание/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Math;
+
+namespace alexandrova2272
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            void BtnOK_Click(object sender, EventArgs e)
+            {
+                double a = Convert.ToDouble(TextBlockAnswer.Text);
+                double s;
+                s = Sqrt(a);
+                TextBlockAnswer.Text = ($"s={a}");
+            }
+            InitializeComponent();
+        }
+
+        private void BtnOK_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
